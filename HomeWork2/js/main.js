74b2ae9b2623250eedf99d41552eccc3c4ee2487
IlyaKavko <58006263+IlyaKavko@users.noreply.github.com>
2020-03-09 16:25:05 +0300

    The second homework

---
+++ HomeWork2/js/main.js
@@ -0,0 +1,56 @@
+// Первое задание
+
+let str = prompt('Введите слово')
+function checkPalindrom(str) {
+    if (str == str.split('').reverse().join('')) {
+        console.log('Это полиндром');
+    } else {
+        console.log('Это не полиндром');
+    }
+}
+checkPalindrom(str);
+
+// Второе задание
+
+function min(a, b) {
+    return a < b ? a : b;
+}
+console.log(min(35,25));
+
+function max(a, b) {
+    return a > b ? a : b;
+}
+console.log(max(834,775));
+
+// Третье задание
+
+function randomIn() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+function getRandom() {
+    let arr = [];
+    for (let i = 0; i < 10; i++) {
+        arr.push(randomIn());
+    }
+    return arr;
+}
+
+
+function getZero(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i] = String(arr[i]);
+      arr[i] = arr[i].split('');
+      for (let j = 0; j < arr[i].length; j++) {
+          if(arr[i][j] === '0') {
+              arr[i][j] = 'zero';
+          }
+      }
+      arr[i] = arr[i].join('');
+    }
+    return arr;
+}
+
+let zeroArr = getRandom();
+console.log(zeroArr);
+console.log(getZero(zeroArr));
