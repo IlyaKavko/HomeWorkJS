b6a78708aa31fb1b3606851f5c1ebc29242da416
IlyaKavko <58006263+IlyaKavko@users.noreply.github.com>
2020-03-20 19:20:56 +0300

    Update

--- HomeWork5/js/main.js
+++ HomeWork5/js/main.js
@@ -3,7 +3,13 @@ let body = document.body
 let table = document.createElement('table');
 body.append(table);
 
-let arr = [['1:1', '1:2', '1:3'], ['2:1', '2:2', '2:3'], ['3:1', '3:2', '3:3']];
+let arr = [
+    ['1:1', '1:2', '1:3', '1:4', '1:5'], 
+    ['2:1', '2:2', '2:3', '2:4', '2:5'], 
+    ['3:1', '3:2', '3:3', '3:4', '3:5'], 
+    ['4:1', '4:2', '4:3', '4:4', '4:5'], 
+    ['5:1', '5:2', '5:3', '5:4', '5:5']
+];
 
 for (let i = 0; i < arr.length; i++) {
     let tr = document.createElement('tr');
@@ -17,6 +23,5 @@ for (let i = 0; i < arr.length; i++) {
 
 for (let k = 0; k < table.rows.length; k++) {
     let rows = table.rows[k];
-    rows.cells[2 - k].classList.add('red')
+    rows.cells[4 - k].classList.add('red')
 }
-
