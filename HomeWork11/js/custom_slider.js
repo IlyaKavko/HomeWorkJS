1bb5ffadcc52044e5c5ea0a1d1cf57b0d6d71c13
IlyaKavko <58006263+IlyaKavko@users.noreply.github.com>
2020-04-15 15:34:59 +0300

    HomeWork11

--- HomeWork11/js/custom_slider.js
+++ HomeWork11/js/custom_slider.js
@@ -1,15 +1,31 @@
 let moveNum = 0;
-let defualMoveNum = 0;
-let defualSlidesOffset = 330;
-let slideItem = 300;
+let defualSliderSetting = {
+    slideWidth: 300,
+    slideHeight: '',
+    maxSlide: 1,
+    loop: false,
+    autoplay: true,
+};
 
 let customSlider = document.querySelector('.custom_slider');
 customSlider.customSlider = function (sliderSetting) {
-    const slidesOffset = sliderSetting.slideWidth;
-    const maxSlide = sliderSetting.maxSlide;
+    const slidesOffset = sliderSetting.slideWidth || defualSliderSetting.slideWidth;
+    const slidesHeight = sliderSetting.slideHeight || defualSliderSetting.slideHeight;
+    const maxSlide = sliderSetting.maxSlide || defualSliderSetting.maxSlide;
+    const loop = sliderSetting.loop || defualSliderSetting.loop;
+    const navs = sliderSetting.navs;
+    const autoplay = defualSliderSetting.autoplay || sliderSetting.autoplay;
+    const slide = customSlider.querySelectorAll('div');
+    const totalLength = slide.length * slidesOffset;
+    
+    autoplayArr();
+    buildSlider(slidesOffset, slidesHeight, slide);
+    navsSeting();
 
-    buildSlider();
-    maxSlideInWindow();
+    let outerContainer = customSlider.querySelector('.outer_container');
+    outerContainer.style.width = `${slidesOffset * maxSlide}px`;
+
+    let innerContainer = document.querySelector('.inner_container');
 
     let nextSlideBtn = document.querySelector('.custom_slider .next_slide');
     let prevSlideBtn = document.querySelector('.custom_slider .prev_slide');
@@ -17,47 +33,67 @@ customSlider.customSlider = function (sliderSetting) {
     prevSlideBtn.addEventListener('click', prevSlideClick);
 
     function nextSlideClick() {
-        moveNum = moveNum - slidesOffset;
-        defualMoveNum = defualMoveNum - defualSlidesOffset;
-        let customSlider = this.closest('.custom_slider');
-        let innerContainer = document.querySelector('.inner_container');
-        if (moveNum) {
+        if ((moveNum - slidesOffset) > -totalLength){
+            moveNum = moveNum - slidesOffset;
             innerContainer.style.transform = `translate(${moveNum}px, 0)`;
-        } else {
-            innerContainer.style.transform = `translate(${defualMoveNum}px, 0)`;
+        } else if (loop == true) {
+            innerContainer.addEventListener('transform', nextJump());
         }
+        return;
     }
 
     function prevSlideClick() {
-        moveNum = moveNum + slidesOffset;
-        defualMoveNum = defualMoveNum + defualSlidesOffset;
-        let customSlider = this.closest('.custom_slider');
-        let innerContainer = document.querySelector('.inner_container');
-        if (moveNum) {
+        if (moveNum < 0) {
+            moveNum = moveNum + slidesOffset;
             innerContainer.style.transform = `translate(${moveNum}px, 0)`;
-        } else {
-            innerContainer.style.transform = `translate(${defualMoveNum}px, 0)`;
+        } else if (loop == true) {
+            innerContainer.addEventListener('transform', prevJump());
+        }
+        return;
+    }
+    
+    function nextJump () {
+        moveNum = 0
+        innerContainer.style.transform = `translate(${moveNum}px, 0)`;
+    }
+
+    function prevJump () {
+        moveNum = -totalLength + slidesOffset;
+        innerContainer.style.transform = `translate(${moveNum}px, 0)`;
+    }
+
+    function navsSeting() {
+        let navsData = document.querySelector('.navs');
+        if (navs == true) {
+            navsData.style.display = "flex";
+        } else if (navs == false){
+            navsData.style.display = "none"; 
         }
     }
 
-    function maxSlideInWindow() {
-        slideItem = slideItem * maxSlide
-        let outerContainer = document.querySelector('.outer_container')
-        outerContainer.style.width = `${slideItem}px`;
-    }
+    function autoplayArr() {
+        if (autoplay == true) {
+            setInterval(() => {
+                nextSlideClick()
+            }, 6000);
+        }
+    };
 }
 
-function buildSlider() {
-    let oneSlideArr = customSlider.querySelectorAll('div');
-    oneSlideArr.forEach(item => item.classList.add('one_slide'))
+function buildSlider(slideWidth, slidesHeight, slide) {
+    slide.forEach(item => {
+        item.classList.add('one_slide');
+        item.style.width = `${slideWidth}px`;
+        item.style.height = `${slidesHeight}px`;
+    })
     customSlider.innerHTML = `
    <div class="outer_container">
             <div class="inner_container">
                 ${customSlider.innerHTML}
             </div>
-        </div>
-        <div class="navs">
-            <a href="#" class="prev_slide"><</a>
-            <a href="#" class="next_slide">></a>
-        </div>`
+    </div>
+    <div class="navs">
+        <a href="#" class="prev_slide"><</a>
+        <a href="#" class="next_slide">></a>
+    </div>`
 }
--- HomeWork11/js/main.js
+++ HomeWork11/js/main.js
@@ -2,4 +2,7 @@ let mySlider = document.querySelector('.custom_slider');
 mySlider.customSlider({
     // slideWidth: 250,
     // maxSlide: 2,
+    loop: true,
+    // navs: false,
+    autoplay: false,
 });
